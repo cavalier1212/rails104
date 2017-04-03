c81499fd82427e5532aeed1f660c3755c1ac526b
cavalier121 <cavalier121@gmail.com>
2017-04-04 02:41:58 +0800

    use simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :destroy, :edit, :update]
+  before_action :samestuff, :only => [:edit, :update, :destroy]
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -18,7 +19,35 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+
+    if current_user != @post.user
+      redirect_to group_path(@group), alert: "You have no permission!"
+    end
+  end
+
+  def update
+
+    @post.update(post_params)
+    redirect_to account_posts_path, notice: "Edit Success!"
+  end
+
+  def destroy
+
+    if @post.destroy
+      flash[:alert] = "Post Delete"
+    redirect_to account_posts_path
+
+    end
+
+  end
+
   private
+
+  def samestuff
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:content)
